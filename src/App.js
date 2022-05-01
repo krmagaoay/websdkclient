163e984e447494822727043e792779006a8eea85
krmagaoay <krm@kelmagaoay.com>
2022-04-30 17:31:44 -0700

    240ss

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   var meetingNumber = new URLSearchParams(window.location.search).get('meetingnumber')
   var role = 0
   var leaveUrl = 'https://zoom.us'
-  var userName = 'CV 240'
+  var userName = 'ClientV 240'
   var userEmail = ''
   var passWord = new URLSearchParams(window.location.search).get('passcode')
   // pass in the registrant's token if your meeting or webinar requires registration. More info here:
